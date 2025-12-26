a02cd8981883c719f7641ab79304f68bd43462dd
Kempes Jacinto <kempes.jacinto@ufrn.br>
2025-12-26 14:03:50 -0600

    Move orders and tracking pages to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 function App() {
 
   return (
       <Routes>
         <Route index element={<HomePage />}/>
-        <Route path="checkout" element={<div>Test </div>}/>
+        <Route path="checkout" element={<CheckoutPage /> }/>
+        <Route path="orders" element={<OrdersPage /> }/>
+        <Route path="tracking" element={<TrackingPage /> }/>
       </Routes>
   )
 }
