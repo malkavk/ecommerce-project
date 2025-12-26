e4ed1b288e9ae64579052657d2393012b05f2c8f
Kempes Jacinto <kempes.jacinto@ufrn.br>
2025-12-26 13:40:05 -0600

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage'
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+      <Routes>
+        <Route index element={<HomePage />}/>
+        <Route path="checkout" element={<div>Test </div>}/>
+      </Routes>
   )
 }
 
